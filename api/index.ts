116cc70794be1e0f4915ec1948ba3a8951d75a42
titanium312 <149283864+titanium312@users.noreply.github.com>
2025-11-25 16:55:47 -0500

    nuevo 3

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,26 @@
+// api/index.ts
 import express, { Request, Response } from 'express';
 import serverless from 'serverless-http';
 
 const app = express();
 
-app.get('/hello', (req: Request, res: Response) => {
-  res.json({ message: 'Hola desde Baken TS en local!' });
+// Ruta principal
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hola desde /api/' });
 });
 
+// Ruta /hello
+app.get('/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hola desde /api/hello en Vercel!' });
+});
+
+// Solo para desarrollo local
+if (process.env.LOCAL !== 'false') {
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Servidor local corriendo en http://localhost:${PORT}`);
+  });
+}
+
+// Exportamos el handler serverless
 export const handler = serverless(app);
